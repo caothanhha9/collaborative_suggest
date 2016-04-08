f21b7d31eee6ebb60326f713d1d1b5986fb86474
hact <hacaothanh@admicro.vn>
2016-04-08 17:03:59 +0700

    update files

--- regression/rnn_predict.py
+++ regression/rnn_predict.py
@@ -5,13 +5,10 @@ import numpy as np
 
 
 class Predict(object):
-    def __init__(self, learning_rate, n_hidden, batch_size, training_iters,
-                 display_step=10):
-        self.learning_rate = learning_rate
+    def __init__(self, n_hidden, batch_size, out_dir):
         self.n_hidden = n_hidden
         self.batch_size = batch_size
-        self.training_iters = training_iters
-        self.display_step = display_step
+        self.ckpt_dir = out_dir + "/checkpoints"
 
     def run(self):
         input_data, label_data = data_helpers.load_data()
@@ -20,8 +17,6 @@ class Predict(object):
         n_classes = 1
         n_hidden = self.n_hidden
         batch_size = self.batch_size
-        training_iters = self.training_iters
-        display_step = self.display_step
         # batches = data_helpers.batch_gen(zip(input_data, label_data), 2)
         # for batch in batches:
         #     x_batch, y_batch = zip(*batch)
@@ -31,42 +26,21 @@ class Predict(object):
 
         new_rnn = RNN(n_steps=n_steps, n_input=n_input,
                       n_hidden=self.n_hidden, n_classes=n_classes)
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(new_rnn.cost) # Adam Optimizer
-        # global_step = tf.Variable(0, name="global_step", trainable=False)
-        # optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        # grads_and_vars = optimizer.compute_gradients(new_rnn.cost)
-        # train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
-        init = tf.initialize_all_variables()
-        with tf.Session() as sess:
-            sess.run(init)
-            step = 1
-            # Keep training until reach max iterations
-            while step * batch_size < training_iters:
-                # batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-                batch_xs, batch_ys = data_helpers.get_random_batch(input_data, label_data, batch_size)
-                # Reshape data to get 28 seq of 28 elements
-                batch_xs = batch_xs.reshape((batch_size, n_steps, n_input))
-                # Fit training using batch data
-                sess.run(optimizer, feed_dict={new_rnn.x: batch_xs, new_rnn.y: batch_ys,
-                                               new_rnn.istate: np.zeros((batch_size, 2*n_hidden))})
-                if step % display_step == 0:
-                    # Calculate batch accuracy
-                    acc = sess.run(new_rnn.accuracy, feed_dict={new_rnn.x: batch_xs, new_rnn.y: batch_ys,
-                                                        new_rnn.istate: np.zeros((batch_size, 2*n_hidden))})
-                    # Calculate batch loss
-                    loss = sess.run(new_rnn.cost, feed_dict={new_rnn.x: batch_xs, new_rnn.y: batch_ys,
-                                                     new_rnn.istate: np.zeros((batch_size, 2*n_hidden))})
-                    print "Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + \
-                          ", Training Accuracy= " + "{:.5f}".format(acc)
-                step += 1
-            print "Optimization Finished!"
-            test_len = 2
-            test_data, test_label = data_helpers.get_random_batch(input_data, label_data, test_len)
-            test_data = test_data.reshape((batch_size, n_steps, n_input))
-            # test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
-            # test_label = mnist.test.labels[:test_len]
-            print "Testing Accuracy:", sess.run(new_rnn.accuracy, feed_dict={new_rnn.x: test_data, new_rnn.y: test_label,
-                                                                             new_rnn.istate: np.zeros((test_len, 2*n_hidden))})
 
-new_predict = Predict(learning_rate=0.001, n_hidden=12, batch_size=2, training_iters=10000)
+        saver = tf.train.Saver(tf.all_variables(), max_to_keep=1)
+        with tf.Session() as sess:
+            ckpt = tf.train.get_checkpoint_state(self.ckpt_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                model_checkpoint_path_arr = ckpt.model_checkpoint_path.split("/")
+                abs_model_checkpoint_path = self.ckpt_dir + '/' + model_checkpoint_path_arr[-1]
+                saver.restore(sess, abs_model_checkpoint_path)
+                test_len = 2
+                test_data, test_label = data_helpers.get_random_batch(input_data, label_data, test_len)
+                test_data = test_data.reshape((batch_size, n_steps, n_input))
+                # test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
+                # test_label = mnist.test.labels[:test_len]
+                print "Testing Accuracy:", sess.run(new_rnn.accuracy, feed_dict={new_rnn.x: test_data, new_rnn.y: test_label,
+                                                                                 new_rnn.istate: np.zeros((test_len, 2*n_hidden))})
+
+new_predict = Predict(n_hidden=12, batch_size=2, out_dir='models')
 new_predict.run()
